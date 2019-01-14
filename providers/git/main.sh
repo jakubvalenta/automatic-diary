d83b4dc69b2177aef37c766f56927daa7461d3ef
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-14 20:13:30 +0100

    git: Read config from environment vars

--- providers/git/main.sh
+++ providers/git/main.sh
@@ -7,10 +7,19 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-base_path="$HOME/devel"
-author="Jakub Valenta"
 date_after=$1
 date_before=$(date -d "$date_after +1 day" "+%Y-%m-%d")
+if [[ -z "$GIT_BASE_PATH" ]]; then
+    echo "Please set the environment variable GIT_BASE_PATH." >&2
+    echo "Example: GIT_BASE_PATH=\"$HOME/devel\"" >&2
+    exit 1
+fi
+
+if [[ -z "$GIT_AUTHOR" ]]; then
+    echo "Please set the environment variable GIT_AUTHOR." >&2
+    echo "Example: GIT_AUTHOR=\"Jakub Valenta\"" >&2
+    exit 1
+fi
 
 echo "Reading Git logs from $date_after to $date_before" >&2
 
@@ -20,7 +29,7 @@ read_repo_log() {
     (
         cd "$repo_path" &&
             (git --no-pager log \
-                --author="$author" \
+                --author="$GIT_AUTHOR" \
                 --after="$date_after" \
                 --before="$date_before" \
                 --format="%ad,git,\"$repo_name\",\"%s\"" \
@@ -28,7 +37,7 @@ read_repo_log() {
     )
 }
 
-for repo_path in "$base_path"/*; do
+for repo_path in "$GIT_BASE_PATH"/*; do
     echo "  Reading repository $repo_path" >&2
     read_repo_log "$repo_path"
 done
