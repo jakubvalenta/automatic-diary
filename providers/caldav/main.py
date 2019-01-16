f00e61e0283fd286424a9edaad9f7672cbef8389
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-16 20:14:19 +0100

    caldav: Parse iCalendar and write CSV

--- providers/caldav/main.py
+++ providers/caldav/main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import csv
+import datetime
+import itertools
 import json
 import logging
 import os
+import re
 import subprocess
 import sys
-from functools import partial
+from functools import partial, reduce
 from pathlib import Path
-from typing import Callable, Iterator
+from typing import Iterable, Iterator, List, Tuple
 
 import caldav
 import ics
@@ -51,16 +55,16 @@ def load_config(path: str) -> dict:
     }
 
 
-def read_data_from_cache(cache_dir: Path) -> Iterator[str]:
+def read_events_data_from_cache(cache_dir: Path) -> Iterator[str]:
     if cache_dir.is_dir():
-        logger.info('Reading cache')
+        logger.info('Using cached events data')
         for cache_file in os.scandir(cache_dir):
             if cache_file.is_file():
                 yield Path(cache_file.path).read_text()
 
 
 def write_events_to_cache(events: Iterator[caldav.Event], cache_dir: Path):
-    logger.info('Writing cache')
+    logger.info('Writing downloaded events data to cache')
     cache_dir.mkdir(parents=True, exist_ok=True)
     for event in events:
         _, event_id = event.url.rsplit('/', maxsplit=1)
@@ -70,13 +74,14 @@ def write_events_to_cache(events: Iterator[caldav.Event], cache_dir: Path):
         cache_file.write_text(event.data)
 
 
-def read_ical(data: str) -> str:
-    pass
-
-
-def download_events(url: str,
-                    username: str,
-                    password: str) -> Iterator[caldav.Event]:
+def download_events(config: dict) -> List[str]:
+    url = config['url']
+    username = config['username']
+    password = config['password']
+    cache_dir = Path(config['cache_dir'])
+    events_data = list(read_events_data_from_cache(cache_dir))
+    if events_data:
+        return events_data
     logger.info('Connecting to %s', url)
     client = caldav.DAVClient(
         url,
@@ -85,8 +90,45 @@ def download_events(url: str,
     )
     logger.info('Reading principal')
     principal = client.principal()
-    for calendar in principal.calendars():
-        yield from calendar.events()
+    events = itertools.chain(
+        calendar.events()
+        for calendar in principal.calendars()
+    )
+    write_events_to_cache(events, cache_dir)
+    return [event.data for event in events]
+
+
+TDateTimeAndText = Tuple[datetime.datetime, str]
+
+
+def parse_ics(events_data: Iterable[str]) -> Iterator[TDateTimeAndText]:
+    for i, event_data in enumerate(events_data):
+        try:
+            calendar = ics.Calendar(event_data)
+        except (ValueError, ics.parse.ParseError):
+            logger.error('Failed to parse event %s', event_data)
+            continue
+        for event in calendar.events:
+            yield (event.begin.datetime, event.name)
+
+
+def format_csv(dt_and_text: Iterable[TDateTimeAndText],
+               provider: str,
+               subprovider: str) -> Iterator[Tuple[str, str, str, str]]:
+    for dt, text in dt_and_text:
+        clean_text = re.sub(r'\s+', ' ', text).strip()
+        yield (
+            dt.strftime('%Y-%m-%d %H:%M:%S %z'),
+            provider,
+            subprovider,
+            clean_text
+        )
+
+
+def chain(*funcs):
+    def wrapped(initializer):
+        return reduce(lambda x, y: y(x), funcs, initializer)
+    return wrapped
 
 
 if __name__ == '__main__':
@@ -96,14 +138,11 @@ if __name__ == '__main__':
         format='%(message)s')
     _, config_path, path_out = sys.argv
     config = load_config(config_path)
-    cache_dir = Path(config['cache_dir'])
-    data = list(read_data_from_cache(cache_dir))
-    if not data:
-        events = list(download_events(
-            config['url'],
-            config['username'],
-            config['password']
-        ))
-        write_events_to_cache(events, cache_dir)
-        data = [event.data for event in events]
-    read_ical(data)
+    with open(path_out, 'a', newline='') as f:
+        writer = csv.writer(f)
+        chain(
+            download_events,
+            parse_ics,
+            partial(format_csv, provider='caldav', subprovider=config['url']),
+            writer.writerows
+        )(config)
