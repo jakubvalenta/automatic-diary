77410f1f5b82dbd10a22091cc2dcab0c65385f28
Jakub Valenta <jakub@jakubvalenta.cz>
2019-01-16 14:24:48 +0100

    caldav: Cache events as separate files
    Because each one is a separate Calendar.

--- providers/caldav/main.py
+++ providers/caldav/main.py
@@ -2,11 +2,12 @@
 
 import json
 import logging
+import os
 import subprocess
 import sys
 from functools import partial
 from pathlib import Path
-from typing import Any, Callable
+from typing import Callable, Iterator
 
 import caldav
 
@@ -36,7 +37,7 @@ def load_config(path: str) -> dict:
         username = config['caldav']['username']
         password_key = config['caldav']['password_key']
         password_val = config['caldav']['password_val']
-        cache_file = config['caldav']['cache_file']
+        cache_dir = config['caldav']['cache_dir']
     except (KeyError, TypeError):
         logger.error('Invalid config')
         sys.exit(1)
@@ -45,28 +46,36 @@ def load_config(path: str) -> dict:
         'url': url,
         'username': username,
         'password': password,
-        'cache_file': cache_file,
+        'cache_dir': cache_dir,
     }
 
 
-def with_cache(func: Callable, cache_file: str) -> Any:
-    path = Path(cache_file)
-    if path.is_file():
+def read_data_from_cache(cache_dir: Path) -> Iterator[str]:
+    if cache_dir.is_dir():
         logger.info('Reading cache')
-        return path.read_text()
-    data = func()
+        for cache_file in os.scandir(cache_dir):
+            if cache_file.is_file():
+                yield Path(cache_file.path).read_text()
+
+
+def write_events_to_cache(events: Iterator[caldav.Event], cache_dir: Path):
     logger.info('Writing cache')
-    path.parent.mkdir(parents=True, exist_ok=True)
-    path.write_text(data)
-    return data
+    cache_dir.mkdir(parents=True, exist_ok=True)
+    for event in events:
+        _, event_id = event.url.rsplit('/', maxsplit=1)
+        cache_file = cache_dir / event_id
+        if cache_file.exists():
+            raise Exception(f'Cache file {cache_file} already exists')
+        cache_file.write_text(event.data)
 
 
 def read_ical(data: str) -> str:
     pass
 
 
-def download_data(url: str, username: str, password: str) -> str:
-    data = []
+def download_events(url: str,
+                    username: str,
+                    password: str) -> Iterator[caldav.Event]:
     logger.info('Connecting to %s', url)
     client = caldav.DAVClient(
         url,
@@ -76,9 +85,7 @@ def download_data(url: str, username: str, password: str) -> str:
     logger.info('Reading principal')
     principal = client.principal()
     for calendar in principal.calendars():
-        for event in calendar.events():
-            data.append(event.data)
-    return '\n'.join(data)
+        yield from calendar.events()
 
 
 if __name__ == '__main__':
@@ -88,13 +95,14 @@ if __name__ == '__main__':
         format='%(message)s')
     _, config_path, path_out = sys.argv
     config = load_config(config_path)
-    data = with_cache(
-        partial(
-            download_data,
+    cache_dir = Path(config['cache_dir'])
+    data = list(read_data_from_cache(cache_dir))
+    if not data:
+        events = list(download_events(
             config['url'],
             config['username'],
             config['password']
-        ),
-        config['cache_file']
-    )
+        ))
+        write_events_to_cache(events, cache_dir)
+        data = [event.data for event in events]
     read_ical(data)
