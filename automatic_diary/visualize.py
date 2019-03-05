dba7ec789186c628804bc26b8a763f2d46f75f91
Jakub Valenta <jakub@jakubvalenta.cz>
2019-03-05 17:40:20 +0100

    visualize: Add option --highlight

--- automatic_diary/visualize.py
+++ automatic_diary/visualize.py
@@ -2,9 +2,11 @@ import argparse
 import csv
 import datetime
 import logging
+import re
 import statistics
 import sys
 from collections import defaultdict
+from functools import partial
 from pathlib import Path
 from typing import Dict, Iterable, Iterator, List
 
@@ -113,16 +115,32 @@ class Year(list):
                 stat.val = round(min(stat.count / mean, 1) * 100)
 
 
-env = Environment(loader=PackageLoader('automatic_diary', 'templates'))
-template = env.get_template('template.html')
+def _matches_regex(s: str, regex: str) -> bool:
+    if not regex:
+        return False
+    return re.search(regex, s) is not None
 
 
-def visualize(csv_path, output_html_path):
-    days = read_days(csv_path)
-    year = Year(days)
-    stream = template.stream(year=year)
+def _render_template(
+    package: List[str], output_html_path: str, highlight: str, **context
+):
+    environment = Environment(loader=PackageLoader(*package[:-1]))
+    environment.tests['highlighted'] = partial(_matches_regex, regex=highlight)
+    template = environment.get_template('template.html')
+    stream = template.stream(**context)
     with Path(output_html_path).open('w') as f:
         f.writelines(stream)
+
+
+def visualize(csv_path: str, output_html_path: str, highlight: str):
+    days = read_days(csv_path)
+    year = Year(days)
+    _render_template(
+        ['automatic_diary', 'templates', 'template.html'],
+        output_html_path,
+        highlight,
+        year=year,
+    )
 
 
 def main():
@@ -132,6 +150,9 @@ def main():
     parser.add_argument('csv_path', help='Input CSV file path')
     parser.add_argument('output_html_path', help='Output HTML file path')
     parser.add_argument(
+        '-i', '--highlight', help='Highlight items matching regex'
+    )
+    parser.add_argument(
         '-v', '--verbose', action='store_true', help='Enable debugging output'
     )
     args = parser.parse_args()
@@ -139,7 +160,7 @@ def main():
         logging.basicConfig(
             stream=sys.stdout, level=logging.INFO, format='%(message)s'
         )
-    visualize(args.csv_path, args.output_html_path)
+    visualize(args.csv_path, args.output_html_path, args.highlight)
 
 
 if __name__ == '__main__':
