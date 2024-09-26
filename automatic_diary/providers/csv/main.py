17a8ecd4a70f642f343b508ff1496fa2c9f28ab1
Jakub Valenta <jakub@jakubvalenta.cz>
2024-09-26 15:50:17 +0200

    csv: remove default all_day

--- automatic_diary/providers/csv/main.py
+++ automatic_diary/providers/csv/main.py
@@ -13,23 +13,22 @@ provider = Path(__file__).parent.name
 
 
 def main(config: dict, *args, **kwargs) -> Iterator[Item]:
-    path = Path(config['path'])
+    path = Path(config["path"])
     subprovider = path.name
-    logger.info('Reading CSV file %s', path)
+    logger.info("Reading CSV file %s", path)
     renderer = pystache.Renderer(escape=lambda u: u)
-    date_source_tmpl = pystache.parse(config['date_source'])
-    text_source_tmpl = pystache.parse(config['text_source'])
+    date_source_tmpl = pystache.parse(config["date_source"])
+    text_source_tmpl = pystache.parse(config["text_source"])
     with path.open() as f:
         reader = csv.DictReader(f)
         for row in reader:
             text = renderer.render(text_source_tmpl, row)
             datetime_ = datetime.datetime.strptime(
-                renderer.render(date_source_tmpl, row), config['date_format']
+                renderer.render(date_source_tmpl, row), config["date_format"]
             )
             yield Item.normalized(
                 datetime_=datetime_,
                 text=text,
                 provider=provider,
                 subprovider=subprovider,
-                all_day=True,
             )
